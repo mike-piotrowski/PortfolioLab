7d261d9d86a310a7139e3ff0dc67f2890feb277d
piotro1994@gmail.com <piotro1994@gmail.com>
2022-04-11 17:26:22 +0200

    Updated HomeHeader

--- src/components/Home/HomeHeader.js
+++ src/components/Home/HomeHeader.js
@@ -1,6 +1,42 @@
-import React from "react";
+import React, { Component } from "react";
 import { Link } from "react-router-dom";
 
-export default function HomeHeader() {
-  return <div>HomeHeader</div>;
+import Navbar from "./Navbar";
+
+class Header extends Component {
+  render() {
+    return (
+      <>
+        <header className="header" id="section1">
+          <div className="header-content">
+            <div className="header-logs">
+              <Link to="/login">Zaloguj się</Link>
+              <Link to="/register">Zarejestruj się</Link>
+            </div>
+            <div className="header-nav">
+              <Navbar />
+            </div>
+          </div>
+
+          <div className="header-text">
+            <div className="header-title">
+              Zacznij pomagać!
+              <br /> Oddaj niechciane rzeczy w zaufane ręce
+            </div>
+            <span className="header-decoration"></span>
+            <div className="header-links">
+              <Link to="/login">
+                Oddaj <br /> rzeczy
+              </Link>
+              <Link to="/register">
+                Zorganizuj <br /> zbiórkę
+              </Link>
+            </div>
+          </div>
+        </header>
+      </>
+    );
+  }
 }
+
+export default Header;
